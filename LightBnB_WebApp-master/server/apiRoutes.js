fb049fdae0d663a9060ccde4a096ce5d0d61b617
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-04 18:39:58 -0500

    Changed the getAllProperties first parameter to access owner_id on database.js

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -1,7 +1,8 @@
 module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
-    database.getAllProperties(req.query, 20)
+    const userId = req.session.userId;
+    database.getAllProperties({...req.query, owner_id: userId}, 20)
     .then(properties => res.send({properties}))
     .catch(e => {
       console.error(e);
