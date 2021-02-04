ed6fccf4f36f4331823025320f4395cd45069c71
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-04 13:14:19 -0500

    Refactored getReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -92,7 +92,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  return pool.query(`
+  SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => res.rows)
+  .catch(e => console.log(e))
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
