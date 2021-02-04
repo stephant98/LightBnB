393ea926206bbf88bcc1205eb1a14700a445c318
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-03 19:21:06 -0500

    Queries for lightbnb database

---
+++ 1_queries/5_all_reservations_user.sql
@@ -0,0 +1,16 @@
+SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
+
+
+
+
+
+
+
